833440439da8fdfe4e6af02761eeafa884dbae36
yura <yura@com.ua>
2018-03-29 20:55:20 +0300

    add to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+spl_autoload_register(function($class) {
+    $filePath = "lib" . DIRECTORY_SEPARATOR . $class . '.php';
+    if (file_exists($filePath)) {
+        include_once $filePath;
+        return true;
+    }
+    return false;
+});
+
+function caller(Testable $obj) {
+    $obj->test_();
+}
+
+//$obj = new Test();
+//caller($obj);
+
+//$obj = new Second();
+//caller($obj);
+
+$obj1 = new Cat('cat', 'siams', 4);
+$obj2 = new Lion('cat', 'african', 10);
+
+$obj1->voice();
+$obj2->voice();
