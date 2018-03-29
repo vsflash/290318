debbb9778e40b3667c41d90a60e6df353df73ff5
yura <yura@com.ua>
2018-03-29 20:55:04 +0300

    add to Cat and Lion

--- lib/Cat.php
+++ lib/Cat.php
@@ -2,10 +2,6 @@
 
 class Cat extends Animals {
     
-    public function __construct($view, $breed, $age) {
-        parent::__construct($view, $breed, $age);
-    }
-    
     public function voice() {
         echo '<p>Мяу! мяу!</p>';
     }
